dc44ce244f0a92f9a0a98b582e5424e6e3ff0877
motpuse-naveen <89586488+motpuse-naveen@users.noreply.github.com>
2022-02-14 19:43:51 +0530

    Show Hide changes

--- 9780702077074_epub/OEBPS/widgets/common_assets/scripts/zoomable_image.js
+++ 9780702077074_epub/OEBPS/widgets/common_assets/scripts/zoomable_image.js
@@ -278,15 +278,4 @@ function enableScroll() {
 
     exports.reset = reset;
 }));
-
 /*** Scroll Pan - END ***/
-
-/*** Disable Right click on image ***/
-var allImageElmts = document.querySelectorAll("figure img");
-allImageElmts.forEach(imgelement => {
-    imgelement.addEventListener("contextmenu", function(event){
-		event.preventDefault();
-		return false;
-	});
-});
-/*** End Disable right click ***/
