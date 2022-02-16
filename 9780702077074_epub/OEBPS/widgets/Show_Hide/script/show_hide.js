3cbbc36b42d5d03ea45936b2d4d40a9891e8c2ab
motpuse-naveen <89586488+motpuse-naveen@users.noreply.github.com>
2022-02-16 21:06:49 +0530

    Remove Zoomable image code as it is out of scope.

--- 9780702077074_epub/OEBPS/widgets/Show_Hide/script/show_hide.js
+++ 9780702077074_epub/OEBPS/widgets/Show_Hide/script/show_hide.js
@@ -18,3 +18,27 @@ $(".show_hide_ans").on('click', function (event) {
 $(document).ready(function () {
     $("div").children(".answer").hide();
 })
+
+cubeData.howMany === 1 ?(smallCubeHolder = (Const.config.SmallCubeFlag === true)? ' small' : ''):(smallCubeHolder = (cubeData.size[0] === 'small') ? ' small' : '');
+
+cubeData.howMany === 1 ? (smallCubeHolder = Const.config.SmallCubeFlag === true ? ' small' : '') : (smallCubeHolder = cubeData.size[0] === 'small' ? ' small' : '');
+
+if(cubeData.howMany === 1){
+    smallCubeHolder = ' small';
+    if(Const.config.SmallCubeFlag === true){
+        smallCubeHolder = ' small';
+    }
+    else{
+        smallCubeHolder ='';
+    }
+
+}
+else{
+    smallCubeHolder = ' small';
+    if(cubeData.size[0] === 'small'){
+        smallCubeHolder = ' small';
+    }
+    else{
+        smallCubeHolder ='';
+    }
+}
